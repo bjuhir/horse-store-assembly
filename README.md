68838406359a69bc514b8eb0ad870dbc0196a22a
bjuhir <binwazidjuhir@gmail.com>
2025-12-07 23:53:13 +0530

    Horse Store in Huff

---
+++ README.md
@@ -0,0 +1,4 @@
+# Horse Store
+
+1. Write a basic simpleStorage / horseStore
+2. Write horseStore in huff
---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.20;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {IHorseStore} from "../../src/horseStoreV1/IHorseStore.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {
+    IHorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = IHorseStore(address(new HorseStore()));
+    }
+
+    function testReadValue() public view {
+        uint256 initialHorses = horseStore.readNumberOfHorses();
+        assertEq(initialHorses, 0, "Initial number of horses should be 0");
+    }
+
+    function testWriteValue(uint256 numberOfHorses) public {
+        horseStore.updateHorseNumber(numberOfHorses);
+        assertEq(horseStore.readNumberOfHorses(), numberOfHorses);
+    }
+}
